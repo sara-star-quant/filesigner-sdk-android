e228ed0e82fe1afd8f7711351d236ba7442c6e38
Peter Z. <pzverkov@users.noreply.github.com>
2026-05-03 11:42:41 +0200

    Broaden dependency force coverage to all configurations and buildscripts via gradle.beforeProject

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,3 +16,35 @@ dependencyResolutionManagement {
 
 rootProject.name = "filesigner-sdk"
 include(":filesigner-core")
+
+val forcedVersions = mapOf(
+    "org.bouncycastle:bcprov-jdk18on"                  to "1.84",
+    "org.bouncycastle:bcpkix-jdk18on"                  to "1.84",
+    "org.bouncycastle:bcutil-jdk18on"                  to "1.84",
+    "io.netty:netty-buffer"                            to "4.1.132.Final",
+    "io.netty:netty-codec"                             to "4.1.132.Final",
+    "io.netty:netty-codec-http"                        to "4.1.132.Final",
+    "io.netty:netty-codec-http2"                       to "4.1.132.Final",
+    "io.netty:netty-codec-socks"                       to "4.1.132.Final",
+    "io.netty:netty-common"                            to "4.1.132.Final",
+    "io.netty:netty-handler"                           to "4.1.132.Final",
+    "io.netty:netty-handler-proxy"                     to "4.1.132.Final",
+    "io.netty:netty-resolver"                          to "4.1.132.Final",
+    "io.netty:netty-transport"                         to "4.1.132.Final",
+    "io.netty:netty-transport-native-unix-common"      to "4.1.132.Final",
+    "org.bitbucket.b_c:jose4j"                         to "0.9.6",
+    "org.jdom:jdom2"                                   to "2.0.6.1",
+    "org.apache.commons:commons-lang3"                 to "3.18.0",
+    "com.squareup.wire:wire-runtime"                   to "5.2.0",
+    "com.squareup.wire:wire-runtime-jvm"               to "5.2.0",
+)
+
+gradle.beforeProject {
+    val applyForce: ResolutionStrategy.() -> Unit = {
+        eachDependency {
+            forcedVersions["${requested.group}:${requested.name}"]?.let { useVersion(it) }
+        }
+    }
+    buildscript.configurations.configureEach { resolutionStrategy(applyForce) }
+    configurations.configureEach { resolutionStrategy(applyForce) }
+}
